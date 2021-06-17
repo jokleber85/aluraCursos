6cffa1dbd0f28dfd5ee145f07970c1a9f37d6c98
John Kleber <jo.kleber85@gmail.com>
2021-06-17 15:27:00 -0300

    Desenvolvimento do projeto até o capítulo 4

--- README.md
+++ README.md
@@ -2,3 +2,66 @@ Alura
 
 TypeScript parte 1: Evoluindo seu Javascript (08 horas)
 ## <br />
+
+Configuração do Arquivo package.json dentro pasta aluraBank</br> 
+```js
+  npm init;
+```
+</br>
+
+Instalação do TypeScript dentro da pasta aluraBank</br>
+```js
+  npm install typescript@2.3.2 --save-dev;
+```
+</br>
+
+* Alterar nome da pasta "js" para "ts"
+* Alterar extensão do arquivo "app.js" para "app.ts"
+* Alterar extensão do arquivo "Negociacao.js" para "Negociacao.ts" 
+
+Criar arquivo de configuração do TypeScript "tsconfig.json"</br>
+```js
+{
+    "compilerOptions": {
+        "target": "es6",
+        "outDir": "js",
+        "noEmitOnError": true, // não deixa gerar arquivo pasta js enquanto houver erro no typescript   
+        "noImplicitAny": true // não deixa adotar o any (tipo nenhum)
+    },
+    "include": [
+        "ts/**/*"
+    ]
+}
+```
+</br>
+
+Configurar compilador para rodar o TypeScript "package.json"</br>
+```js
+{
+  "name": "alurabank",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "directories": {
+    "lib": "lib"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "compile": "tsc",
+    "start": "tsc -w"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "typescript": "^2.3.2"
+  }
+}
+```
+</br>
+
+Rodar o compilador</br>
+```js
+// npm run compile
+npm start
+```
+</br>
---
+++ aluraBank/ts/controllers/NegociacaoControllers.ts
@@ -0,0 +1,39 @@
+class NegociacaoController {
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView("#negociacoesView");
+    private _mensagemView = new MensagemView("#mensagemView");
+
+    constructor() {
+        this._inputData = <HTMLInputElement>document.querySelector("#data");
+        this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade");
+        this._inputValor = <HTMLInputElement>document.querySelector("#valor");
+        this._negociacoesView.update(this._negociacoes);
+    }
+
+    adiciona(event: Event): void {
+        
+        event.preventDefault();
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        this._negociacoes.adiciona(negociacao) ;
+        //console.log(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update("Negociação adicionada com sucesso"); 
+
+/*      this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        }) */
+
+    }
+}
+
